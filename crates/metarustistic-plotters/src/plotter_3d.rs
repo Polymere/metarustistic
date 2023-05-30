1feec097077276ea81ee75b70bf9dc636fd38d49
prevel <paul.prevel@protonmail.com>
2023-05-30 21:23:34 +0200

    added surface plot + fix axis

--- crates/metarustistic-plotters/src/plotter_3d.rs
+++ crates/metarustistic-plotters/src/plotter_3d.rs
@@ -1,4 +1,7 @@
 use plotters::prelude::*;
+use crate::{
+    functions::rastrigin,
+};
 pub fn plot_traj(values:Vec<Vec<f64>>){
     let root = BitMapBackend::new("/home/paul/3d-line.png", (640, 480)).into_drawing_area();
 
@@ -7,15 +10,25 @@ pub fn plot_traj(values:Vec<Vec<f64>>){
     let mut chart = ChartBuilder::on(&root)
         .margin(20)
         .caption("3D Line", ("sans-serif", 40))
-        .build_cartesian_3d(-5.12..5.12 as f64, 0.0..20.0 as f64, -5.12..5.12 as f64)
+        .build_cartesian_3d(-5.12..5.12 as f64, 0.0..10.0 as f64, -5.12..5.12 as f64)
         .unwrap();
+    chart.with_projection(|mut pb| {
+        pb.yaw = 0.5;
+        pb.scale = 0.9;
+        pb.into_matrix()
+    });
     chart.configure_axes().draw().unwrap();
-
-    // let x_iter= values.iter().map(|p| p[0]).collect();
-    // let y_iter = values.iter().map(|p| p[1]).collect();
-    // let z_iter= values.iter().map(|p|p[2]).collect();
+    chart
+        .draw_series(
+            SurfaceSeries::xoz(
+                (-51..51).map(|f| f as f64/10.0),
+                (-51..51).map(|f| f as f64/10.0),
+                |x,z| rastrigin(&vec![x as f32,z as f32])as f64,
+            )
+            .style(BLUE.mix(0.1).filled()),
+        ).unwrap();
     chart.draw_series(LineSeries::new(
-        values.iter().map(|p| (p[0],p[1],p[2])),
+        values.iter().map(|p| (p[0],p[2],p[1])),
         &RED
     )).unwrap();
 }
